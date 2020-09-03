c05c288b6ba234098976ffe49b63e67e86d8be1c
Vladimir Skoblo <rregacc@gmail.com>
2020-09-03 11:48:18 +0300

    added postproccess for 2019.3+

---
+++ Assets/Editor/CoreHapticPostProcessor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+#if UNITY_EDITOR && UNITY_IOS
+
+public class CoreHapticPostProcessor
+{
+    [PostProcessBuild(1)]
+    public static void OnPostProcessBuild(BuildTarget target, string pathToProject)
+    {
+        var projectPath = PBXProject.GetPBXProjectPath(pathToProject);
+
+        var pbxproj = new PBXProject();
+        pbxproj.ReadFromFile(projectPath);
+
+        var guid = pbxproj.GetUnityFrameworkTargetGuid();
+        pbxproj.AddFrameworkToProject(guid, "CoreHaptics.framework", true);
+
+        pbxproj.WriteToFile(projectPath);
+    }
+}
+
+#endif
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # CoreHapticUnityPlugin
 Unity iOS plugin for Core Haptic API
 
+PostProcess temporary work only for Unity 2019.3+
+
 *Plugin currently is under development*
 
 This plugin allows to use new Core Haptic API in iOS 13+
@@ -13,4 +15,3 @@ Depends on Json.Net (Uses Asset Store version)
 
 ToDo:
 - Play from AHAP file
-
